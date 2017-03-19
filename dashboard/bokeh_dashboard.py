d85a2565c04dbcd302e2113aefb48f595b056576
Kaan Divringi <kdivringi@gmail.com>
2017-03-18 19:34:15 -0700

    Pulling the data frome the cloud now!

--- dashboard/bokeh_dashboard.py
+++ dashboard/bokeh_dashboard.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from bokeh.layouts import layout, Spacer
 from bokeh.models import ColumnDataSource
 from bokeh.plotting import figure, curdoc
+from db import user_walker
 
 # RUN bokeh serve --show bokeh_dashboard.py
 
@@ -10,7 +11,8 @@ from bokeh.plotting import figure, curdoc
 rri_plot = figure(x_axis_label="Time",
                   x_axis_type="datetime",
                   y_axis_label="RRI",
-                  toolbar_location=None)
+                  toolbar_location=None,
+                  y_range=(0,1200))
 
 
 diag_plot = figure(toolbar_location=None,
@@ -19,18 +21,26 @@ diag_plot = figure(toolbar_location=None,
 
 
 def gen_final_data():
-    # Timespec argument errored out for me. Maybe need to update Python?
-    new_t = datetime.now()  # .isoformat()#timespec='milliseconds')
-    new_RRI = 600 + 600 * random()
-    return (new_t, new_RRI)
+  # Timespec argument errored out for me. Maybe need to update Python?
+  new_t = datetime.now()  # .isoformat()#timespec='milliseconds')
+  new_RRI = 600 + 600 * random()
+  return (new_t, new_RRI)
 
 
-initial_data = [gen_final_data() for i in range(1)]
-start_x, start_y = list(zip(*initial_data))
+def calc_alphas(column):
+  interval = 1 / len(column)
+  alphas = [interval * i for i in range(1, 1 + len(column))]
+  return alphas
 
 
-source = ColumnDataSource(data=dict(x=start_x, y=start_y))
-source_mask = ColumnDataSource(data=dict(prev=[], cur=[], alphas=[]))
+stream = user_walker()
+
+start_x, start_y = zip(*next(stream))
+
+
+source = ColumnDataSource(data=dict(x=list(start_x), y=list(start_y)))
+source_mask = ColumnDataSource(data=dict(
+    prev=start_y[-11:-1], cur=start_y[-10:], alphas=calc_alphas(start_y[-10:])))
 
 rri_plot.line(x='x', y='y', source=source)
 diag_plot.line(x=(0, 1200), y=(0, 1200),
@@ -44,21 +54,16 @@ doc = curdoc()
 # def gen_input_data():
 
 
-def calc_alphas(column):
-    interval = 1 / len(column)
-    alphas = [interval * i for i in range(1, 1 + len(column))]
-    return alphas
-
-
 def update():
-    new_x, new_y = gen_final_data()
-    source.stream(dict(x=[new_x], y=[new_y]), rollover=10000)
+    new_x, new_y = zip(*next(stream))
+    source.stream(dict(x=list(new_x), y=list(new_y)), rollover=10000)
     if len(source_mask.data['prev']) == 0:
-        source_mask.stream(dict(prev=[new_y], cur=[new_y], alphas=[1]))
+      source_mask.stream(dict(prev=[new_y], cur=[new_y], alphas=[1]))
     else:
-        prev = source_mask.data['cur'][-1]
-        source_mask.stream(
-            dict(prev=[prev], cur=[new_y], alphas=[1]), rollover=10)
+      num_new = len(new_y)
+      prev = source_mask.data['cur'][-num_new - 1:-1]
+      source_mask.stream(
+          dict(prev=prev, cur=list(new_y), alphas=[1] * num_new), rollover=10)
     source_mask.data['alphas'] = calc_alphas(source_mask.data['cur'])
 
 
